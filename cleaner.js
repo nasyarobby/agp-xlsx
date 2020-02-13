58750cc79834a421636d961fda1f99f921148042
Nasyarobby <nasyarobby@gmail.com>
2020-02-13 16:42:15 +0700

    fixed bugs

--- cleaner.js
+++ cleaner.js
@@ -1,6 +1,6 @@
 module.exports = {
   stripAtSign: str => {
-    str = str.trim();
+    str = str.toString().trim();
     str = str[0] === "@" ? str.substring(1, str.length) : str;
     return str;
   }
